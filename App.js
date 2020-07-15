249feddbdaae098dd9df3d608b5da61c2e277fcb
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-15 13:02:11 -0300

    Field Base

--- App.js
+++ App.js
@@ -2,13 +2,23 @@ import React, {Component} from 'react';
 import {Text, SafeAreaView, StyleSheet} from 'react-native';
 
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
 	render() {
 		return (
 			<SafeAreaView style={styles.container}>
 				<Text>Oi</Text>
-				<Text>{params.getColumnsAmount()}x{params.getRowsAmount()}</Text>
+				<Text>
+					{params.getColumnsAmount()}x{params.getRowsAmount()}
+				</Text>
+				<Field />
+				<Field opened />
+				<Field mined />
+				<Field opened nearMines={1} />
+				<Field opened nearMines={2} />
+				<Field opened nearMines={3} />
+				<Field opened nearMines={6} />
 			</SafeAreaView>
 		);
 	}
