33f6862b9a84860fcdc89d2cf3cba67d03145ab3
Anthony Cruz <anthonyleierlw@gmail.com>
2020-07-16 15:09:14 -0300

    Níveis de Dificuldade

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import params from './src/params';
 import {createMinedBoard, cloneBoard, openField, hasExplosion, wonGame, showMines, invertFlag, flagsUsed} from './src/engine';
 import Minefield from './src/components/Minefield';
 import Header from './src/components/Header';
+import LevelSelection from './src/screens/LevelSelection';
 
 export default class App extends Component {
 	constructor(props) {
@@ -25,6 +26,7 @@ export default class App extends Component {
 			board: createMinedBoard(rows, cols, this.minesAmount()),
 			won: false,
 			lost: false,
+			showLevelSelection: false,
 		};
 	};
 
@@ -57,12 +59,23 @@ export default class App extends Component {
 		this.setState({board, won});
 	};
 
+	onLevelSelected = level => {
+		params.difficultyLevel = level;
+		this.setState(this.createState());
+	};
+
 	render() {
 		return (
 			<SafeAreaView style={styles.container}>
+				<LevelSelection
+					isVisible={this.state.showLevelSelection}
+					onLevelSelected={this.onLevelSelected}
+					onCancel={() => this.setState({showLevelSelection: false})}
+				/>
 				<Header
 					flagsLeft={this.minesAmount() - flagsUsed(this.state.board)}
 					onNewGame={() => this.setState(this.createState())}
+					onFlagPress={() => this.setState({showLevelSelection: true})}
 				/>
 				<View style={styles.board}>
 					<Minefield board={this.state.board} onOpenField={this.onOpenField} onSelectField={this.onSelectField} />
